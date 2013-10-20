035aa115664487f8dfa9f384961568b925d62e96
Sheldon Porcina <sheldon_porcina@gmail.com>
2013-10-20 10:31:20 -0600

    Removed commented code

--- src/test/java/org/mule/modules/jbehave/JBehaveStories.java
+++ src/test/java/org/mule/modules/jbehave/JBehaveStories.java
@@ -1,12 +1,5 @@
 package org.mule.modules.jbehave;
 
-/**
- * Created with IntelliJ IDEA.
- * User: sporcina
- * Date: 9/25/13
- * Time: 9:31 AM
- * To change this template use File | Settings | File Templates.
- */
 
 import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
 import org.jbehave.core.configuration.Configuration;
@@ -25,76 +18,25 @@ import org.mule.modules.samples.FakeCustomer;
 
 import java.util.List;
 
-// credit to: https://blog.codecentric.de/en/2012/06/jbehave-configuration-tutorial/
 
+/**
+ * Configures the JBehave ecosystem to run stories
+ */
 @RunWith(JUnitReportingRunner.class)
 public class JBehaveStories extends JUnitStories {
 
-    //private Configuration configuration;
     private static final String STORIES = "**/*.story";
     private static final Integer TEN_MINUTES_AS_SECONDS = 600;
 
+
     public JBehaveStories() {
         super();
-
-        /* TODO: this works, but does not display the story information in an attractive manner, like this:
-
-            (org/mule/modules/jbehave/stories/StorageManagement.story)
-            Scenario: Create a DynamoDB repository
-            Given I have DynamoDB credentials
-            When I request a new repository
-            Then the repository is created successfully (FAILED)
-
-            This is what the current config produces, but it is not as easy to configure.
-            Need to play with this more to find the right settings.
-        */
-
-        /*configuration = new Configuration() {};
-
-        // configuration.doDryRun(false); "no dry run" is implicit by using
-        // default StoryControls
-
-        // configuration.useDefaultStoryReporter(new ConsoleOutput());
-        // deprecated -- rather use StoryReportBuilder
-
-        configuration.useFailureStrategy(new RethrowingFailure());
-        configuration.useKeywords(new LocalizedKeywords(Locale.ENGLISH));
-        configuration.usePathCalculator(new AbsolutePathCalculator());
-        configuration.useParameterControls(new ParameterControls());
-        configuration.useParameterConverters(new ParameterConverters());
-        configuration.useParanamer(new NullParanamer());
-        configuration.usePendingStepStrategy(new FailingUponPendingStep());
-        configuration.useStepCollector(new MarkUnmatchedStepsAsPending());
-        configuration.useStepdocReporter(new PrintStreamStepdocReporter());
-        configuration.useStepFinder(new StepFinder());
-        configuration.useStepMonitor(new PrintStreamStepMonitor());
-        //orig: configuration.useStepMonitor(new SilentStepMonitor());
-        configuration
-                .useStepPatternParser(new RegexPrefixCapturingPatternParser());
-        configuration.useStoryControls(new StoryControls());
-        configuration.useStoryLoader(new LoadFromClasspath());
-        configuration.useStoryParser(new RegexStoryParser(configuration
-                .keywords()));
-        configuration.useStoryPathResolver(new UnderscoredCamelCaseResolver());
-        configuration.useStoryReporterBuilder(new StoryReporterBuilder().withFormats(Format.STATS));
-        configuration.useViewGenerator(new FreemarkerViewGenerator());
-
-        EmbedderControls embedderControls = configuredEmbedder()
-                .embedderControls();
-        embedderControls.doBatch(false);
-        embedderControls.doGenerateViewAfterStories(true);
-        embedderControls.doIgnoreFailureInStories(false);
-        embedderControls.doIgnoreFailureInView(false);
-        embedderControls.doSkip(false);
-        embedderControls.doVerboseFailures(false);
-        embedderControls.doVerboseFiltering(false);
-        embedderControls.useStoryTimeoutInSecs(300);
-        embedderControls.useThreads(1);*/
 
         // TODO: review how this works internally - sporcina (Oct.13,2013)
         EmbedderControls embedderControls = configuredEmbedder().embedderControls();
         embedderControls.useStoryTimeoutInSecs(TEN_MINUTES_AS_SECONDS);
     }
+
 
     @Override
     public InjectableStepsFactory stepsFactory() {
@@ -107,14 +49,15 @@ public class JBehaveStories extends JUnitStories {
         );
     }
 
+
     @Override
     public Configuration configuration() {
-        //return configuration;
         return new MostUsefulConfiguration()
                 .useStoryLoader(new LoadFromClasspath(this.getClass()))
                 .useStoryReporterBuilder(new StoryReporterBuilder()
                         .withFormats(Format.XML, Format.IDE_CONSOLE, Format.CONSOLE, Format.HTML, Format.TXT));
     }
+
 
     @Override
     protected List<String> storyPaths() {
